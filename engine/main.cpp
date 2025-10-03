f832b630f149e29ff983b2298eca487da626e6fb
laithaustin <laithaustin@gmail.com>
2025-10-02 20:58:13 -0400

    more typing

--- engine/main.cpp
+++ engine/main.cpp
@@ -10,13 +10,23 @@
 // observation space: board(24x18), current tetromino mask, stored piece, upcoming piece
 // rewards: +1 clear line, -2 when the game is over, .001 per step alive?
 
-enum Action : uint8_t {
+enum class Action : uint8_t {
    LEFT, RIGHT, DOWN, CW, CCW, DROP, SWAP, NOOP
+};
+
+struct Observation {
+   static constexpr int BoardW = 18;
+   static constexpr int BoardH = 24;
+
+    std::vector<std::vector<uint8_t> > board;
+    std::vector<std::vector<uint8_t> > active_tetromino;
+    std::vector<std::vector<uint8_t> > holder;
+    std::vector<std::vector<uint8_t> > queue;
 };
 
 class TetrisGame {
 public:
-    TetrisGame(TimeManager::Mode m, uint8_t queue_size);
+    TetrisGame(TimeManager::Mode m, uint8_t queue_size = 3);
 
     void reset();
     bool step(int action);
@@ -39,17 +49,18 @@ private:
     int score;
     bool game_over;
     TimeManager tm;
+    Observation obs;
 };
 
-TetrisGame::TetrisGame(TimeManager::Mode m, uint8_t queue_size = 3): tm(TimeManager(m)) {
-    board.resize(Tetris::BOARD_HEIGHT, std::vector<uint8_t>(Tetris::BOARD_WIDTH, 0));
-    active_tetromino.resize(Tetris::BOARD_HEIGHT, std::vector<uint8_t>(Tetris::BOARD_WIDTH, 0));
-    holder.resize(Tetris::PIECE_SIZE, std::vector<uint8_t>(Tetris::PIECE_SIZE, 0));
-    queue.resize(queue_size * Tetris::PIECE_SIZE, std::vector<uint8_t>(Tetris::PIECE_SIZE, 0));
+TetrisGame::TetrisGame(TimeManager::Mode m, uint8_t queue_size): tm(TimeManager(m)) {
+    board.resize(Observation::BoardH, std::vector<uint8_t>(Observation::BoardW, 0));
+    active_tetromino.resize(Observation::BoardH, std::vector<uint8_t>(Observation::BoardW, 0));
+    // holder.resize(Tetris::PIECE_SIZE, std::vector<uint8_t>(Tetris::PIECE_SIZE, 0));
+    // queue.resize(queue_size * Tetris::PIECE_SIZE, std::vector<uint8_t>(Tetris::PIECE_SIZE, 0));
 }
 
 bool TetrisGame::step(int action) {
-    // TODO
+    // TODO: needs to essentially
     return true;
 }
 
@@ -64,21 +75,21 @@ int TetrisGame::getAction() {
     std::cin >> input;
 
     if (input == "j") {
-        return LEFT;
+        return static_cast<int>(Action::LEFT);
     } else if (input == "l") {
-        return RIGHT;
+        return static_cast<int>(Action::RIGHT);
     } else if (input == "k") {
-        return DOWN;
+        return static_cast<int>(Action::DOWN);
     } else if (input == "o") {
-        return CW;
+        return static_cast<int>(Action::CW);
     } else if (input == "u") {
-        return CCW;
+        return static_cast<int>(Action::CCW);
     } else if (input == "K") {
-        return DROP;
+        return static_cast<int>(Action::DROP);
     } else if (input == ",") {
-        return SWAP;
+        return static_cast<int>(Action::SWAP);
     } else {
-        return NOOP;
+        return static_cast<int>(Action::NOOP);
     }
 }
 
